7167b575cf7152d24e1f4969a839a21b21edf292
nithyaGK <nithi.coolz@gmail.com>
2023-03-28 18:11:27 -0500

    first comit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Header from './Header'
+import Footer from './Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <><div> <Header /> </div>
+    <div><Footer/></div>
+    </>
   );
+
 }
 
 export default App;
+
---
+++ src/Header/Header.js
@@ -0,0 +1,9 @@
+ import React from 'react'
+ import './Header.css'
+ 
+ export const Header = () => {
+   return (
+     <div className='header text-center bg-primary text-white'>My First React Application </div>
+   )
+ }
+ 
